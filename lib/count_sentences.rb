fd8241155fa6202abb4bcc365c3789c233519f70
Alausa Aishat <qtaisha.ao@gmail.com>
2022-08-28 17:25:09 +0100

    lab completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # if self.end_with?(".")
+    # #   return true
+    # # else
+    # #   return false
+    # end
+    self.end_with?(".")
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[\.\?\!]/)
+    new_array = sentence_array = sentence_array.reject {|s| s == ""}
+    new_array.size
   end
 end
